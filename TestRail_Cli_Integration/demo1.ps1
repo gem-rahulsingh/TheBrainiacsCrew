28d5d69c954a0c2105cb91f36795b719532b0c08
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2022-12-16 15:23:28 +0530

    Add files via upload

---
+++ TestRail_Cli_Integration/demo1.ps1
@@ -0,0 +1,8 @@
+  trcli -y `
+   -h "https://dipanshu0982.testrail.io//" `
+   -u "dipanshu.kapoor@geminisolutions.com" `
+   -p "ig9oOljU/CpK7KivR88s" `
+   --project "Gemini Login suite" `
+    parse_junit `
+    --title "Automated Tests Run" `
+    -f "demo.xml"
